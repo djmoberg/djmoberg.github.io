37c4c954e73f76c02a17aeee2da96c1a8ac60c57
Daniel <daniel@moberg.no>
2019-01-26 16:53:04 +0100

    build

---
+++ precache-manifest.a929340a16b456b13e81f68ed9e62406.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb89d12c65ffc7bab51e",
+    "url": "/static/js/main.eb89d12c.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "eb89d12c65ffc7bab51e",
+    "url": "/static/css/main.53f09a6b.chunk.css"
+  },
+  {
+    "revision": "595fe2c62a710e847c14a03357a0e8bd",
+    "url": "/index.html"
+  }
+];
